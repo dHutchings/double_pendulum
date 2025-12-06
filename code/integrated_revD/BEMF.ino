75c4f926a993e011d65ea3c63f668876b15371a3
dHutchings <11douglash@berkeley.edu>
2025-12-05 20:23:36 -0800

    Add BEMF sensing statistics for the outstreched arm condition

--- code/integrated_revD/BEMF.ino
+++ code/integrated_revD/BEMF.ino
@@ -1,3 +1,7 @@
+#include "RunningAverage.h"
+RunningAverage BEMF_History(10);  //gets data sample of last 10 BEMFs. / when I should have pushed
+
+
 void setup_BEMF_sensing()
 {
   pinMode(a_peak_rst,INPUT); // dont reset the A_peak_sensing
@@ -6,14 +10,39 @@ void setup_BEMF_sensing()
   //per the datasheet, 7.8.6. we need to explicitly disable the digital input buffer since we're using it as an analog port.
   //This change makes little difference at rest .. but it's thing to do according to the datasheet.
 
+  BEMF_History.fillValue(1024,10);
+
 }
 
 float measure_BEMF()
 {
   int val = analogRead(a_peak); 
+  BEMF_History.add(float(val));
 
-  float v_peak = float(val)*float(5000)/float(1024); //I like millivolts, not volts.
-  return v_peak;
+  #if SPEED_DEBUG_PRINTS
+  Serial.print("Speed:");
+  Serial.print(val);
+  Serial.print(",Avg:");
+  Serial.print(BEMF_History.getFastAverage());
+  Serial.print(",Diff:");
+  Serial.println(BEMF_History.getMaxInBuffer() - BEMF_History.getMinInBuffer());
+  #endif
+
+  //if there isn't a lot of variation in my hisotyr
+  //and if I am going fast.
+  if( (BEMF_History.getMaxInBuffer() - BEMF_History.getMinInBuffer()) < 70 && BEMF_History.getFastAverage() < 150)
+  {
+    #if SPEED_DEBUG_PRINTS
+    Serial.println("Detected Swinging In perpetual loop, straight arm");
+    #endif
+  }
+  
+
+
+  return float(val);
+
+  //float v_peak = float(val)*float(5000)/float(1024); //I like millivolts, not volts.
+  //return v_peak;
   //return 2000 - v_peak; //Probably, in final controls mode, I would like the sign reversed so larger is faster.  But right now, return the number I can confirm with o-scope
 }
 
